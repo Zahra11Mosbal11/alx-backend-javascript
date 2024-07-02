2226c1e149d761134da462e0769054556d7f9819
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-07-02 15:53:28 +0000

    try

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -7,12 +7,13 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
 
   return students
     .filter((student) => student.location === city)
-    .map((student) => ({
-      id: student.id,
-      firstName: student.firstName,
-      location: student.location,
-      grade: (newGrades
-        .filter((grade) => grade.studentId === student.id)
-        .pop() || defaultGrade).grade,
-    }));
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id) || defaultGrade;
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: gradeObj.grade,
+      };
+    });
 }

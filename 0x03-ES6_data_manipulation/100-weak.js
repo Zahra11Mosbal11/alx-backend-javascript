8ec285431209c7ad1e01a60ccf8de1a792e393de
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-07-02 19:27:47 +0000

    try

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+export const weakMap = new WeakMap();
+const MAX_LODE = 5;
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_LODE) {
+    throw new Error('Endpoint load is high');
+  }
+}
--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,17 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const defaultGrade = { grade: 'N/A' };
 
-  if (!Array.isArray(students)) {
-    return [];
-  }
-
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const gradeObj = newGrades.find((grade) => grade.studentId === student.id) || defaultGrade;
-      return {
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
         id: student.id,
         firstName: student.firstName,
         location: student.location,
-        grade: gradeObj.grade,
-      };
-    });
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
 }

8c1d78ecf5c0d97c0992775fefe311bf4fc78494
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-08-20 05:47:18 +0000

    pls

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,17 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 });
 
 rl.question("Welcome to Holberton School, what is your name?\n", function(name) {
 	console.log(`Your name is: ${name}`);
 
-	console.log("This important software is now closing");
 	rl.close();
+});
+
+rl.on('close', function() {
+	console.log("This important software is now closing");
+	process.exit(0);
 });

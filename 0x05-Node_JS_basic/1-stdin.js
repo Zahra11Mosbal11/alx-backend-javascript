96de9666b0fa474790a2d84a112c59912f69af11
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-08-20 16:45:06 +0000

    test

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,13 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-rl.question("Welcome to Holberton School, what is your name?\n", function(name) {
-	console.log(`Your name is: ${name}`);
-
-	rl.close();
-});
-
-rl.on('close', function() {
-	console.log("This important software is now closing");
-	process.exit(0);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
